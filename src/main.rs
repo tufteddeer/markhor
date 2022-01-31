eb05938d47c24a5f49dd3620e2271745d4e071a2
TuftedDeer <tufteddeer@disroot.org>
2022-01-31 17:01:24 +0100

    basic template rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use tera::{Context, Tera};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let tera = Tera::new("templates/**/*.html").expect("failed to parse template");
+
+    let mut context = Context::new();
+
+    context.insert("name", "world");
+
+    let animals = vec!["cat", "dog", "horse"];
+    context.insert("animals", &animals);
+    let result = tera.render("hello.html", &context)?;
+
+    println!("{}", result);
+    Ok(())
 }
