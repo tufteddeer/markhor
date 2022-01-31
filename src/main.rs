443c6194fbdc9da6bc9e320398f7fa7890ab94ec
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2022-01-31 17:28:53 +0100

    move tera rendering to lib fn

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 use std::error::Error;
 
-use tera::{Context, Tera};
+use rust_templating::render_template;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let tera = Tera::new("templates/**/*.html").expect("failed to parse template");
-
-    let mut context = Context::new();
-
-    context.insert("name", "world");
-
-    let animals = vec!["cat", "dog", "horse"];
-    context.insert("animals", &animals);
-    let result = tera.render("hello.html", &context)?;
-
+    let result = render_template()?;
     println!("{}", result);
     Ok(())
 }
