c30c17d4c85382e7f4a497cf0bdbbce8e8aa9c6f
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2022-02-01 17:25:20 +0100

    measure time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{ffi::OsString, path::Path};
+use std::{ffi::OsString, ops::Sub, path::Path, time::Instant};
 
 use rust_templating::{convert_posts, render_index, write_output};
 use simple_logger::SimpleLogger;
@@ -11,11 +11,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let posts_dir = Path::new("markdown");
     let output_dir = Path::new("out");
 
+    let start_time = Instant::now();
+
     let post_metadata = convert_posts(posts_dir, output_dir)?;
 
     let index_html = render_index(&post_metadata)?;
 
     write_output(output_dir, &OsString::from("index.html"), index_html)?;
+
+    let elapsed_time = Instant::now().sub(start_time);
+    log::info!("Took {}ms", &elapsed_time.as_millis());
 
     Ok(())
 }
